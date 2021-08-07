47256aeba46eae3d85ddd9f2a147ed6b9ad022f6
Siddhesh M <31335947+SId02@users.noreply.github.com>
2021-08-07 15:27:23 +0530

    update
    add an update

--- ncovid2019Tracker-global/js/script.js
+++ ncovid2019Tracker-global/js/script.js
@@ -5,37 +5,28 @@ async function getCovidapi() {
   const jsdata = await jsondata.json();
   var globaldata = jsdata.Global;
     let globalDashboard = `
-  
-                    <div class="pb-5 m-auto row row-cols-1 row-cols-md-4 g-4">
+                    <div class="pb-5 m-auto row row-cols-1 row-cols-md-3 g-3">
                     <div class="col">
-                      <div class="card border-3 border-secondary ">
+                      <div class="card cardborderone ">
                         <div class="text-center card-body">
-                          <h5 class="card-title">Confirmed </h5>
-                          <p class="card-text">${globaldata.confirmed}</p>
+                          <h5 class="card-title text-uppercase">Confirmed </h5>
+                          <p class="card-text">${globaldata.TotalConfirmed}</p>
                         </div>
                       </div>
                     </div>
                     <div class="col">
-                      <div class="card border-3 border-warning">
-                        <div class="text-center card-body">
-                          <h5 class="card-title">Active </h5>
-                          <p class="card-text">${globaldata.active}</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col">
-                      <div class="card border-3 border-success">
+                      <div class="card cardborderthree">
                       <div class="text-center card-body">
-                          <h5 class="card-title">Recovered </h5>
-                          <p class="card-text">${globaldata.recovered}</p>
+                          <h5 class="card-title text-uppercase">Recovered </h5>
+                          <p class="card-text">${globaldata.TotalRecovered}</p>
                         </div>
                       </div>
                     </div>
                     <div class="col">
-                      <div class="card border-3 border-danger">
+                      <div class="card cardborderfour">
                         <div class="text-center card-body">
-                          <h5 class="card-title">Deaths</h5>
-                          <p class="card-text">${globaldata.deaths}</p>
+                          <h5 class="card-title text-uppercase">Deaths</h5>
+                          <p class="card-text">${globaldata.TotalDeaths}</p>
                         </div>
                       </div>
                     </div>
@@ -49,7 +40,6 @@ getCovidapi();
 
 
 
-
 /* Countries list */
 const op = document.querySelector("#op");
 async function getData() {
@@ -58,44 +48,57 @@ async function getData() {
   const json = await response.json();
   json.Countries.forEach((countryDetail) => {
     op.innerHTML += `
-				 <div class="row row-cols-1 row-cols-md-3 g-3">
-        <div class="col">
-          <div class="card h-100">
+				<div class="col-lg-4 col-md-3 g-3">
+      <div class="card h-100">
             <div class="card-body">
               <h5 class="card-title text-center">${countryDetail.Country}</h5>
-              <p class="card-text"><ul class="list-group">
-                      <li class="list-group-item">
-                        Country Code: ${countryDetail.CountryCode}
-                      </li>
-                      <li class="list-group-item list-group-item-warning">
-                        New Confirmed: ${countryDetail.NewConfirmed}
-                      </li>
-                      <li class="list-group-item list-group-item-success">
-                        New Recovered: ${countryDetail.NewRecovered}
-                      </li>
-                      <li class="list-group-item list-group-item-danger">
-                        New Deaths: ${countryDetail.NewDeaths}
-                      </li>
-                      <li class="list-group-item">
-                        Total Confirmed: <span class="badge badge-pill badge-warning"> ${countryDetail.TotalConfirmed}</span>
-                      </li>
-                      <li class="list-group-item">
-                        Total Deaths: <span class="badge badge-pill badge-danger">${countryDetail.TotalDeaths}</span>
-                      </li>
-                      <li class="list-group-item">
-                        Total Recovered: <span class="badge badge-pill badge-success"> ${countryDetail.TotalRecovered} </span>
-                      </li>
-                    </ul></p>
-                    <div class="card-footer  text-center">
-                  <small class="text-muted">Date :  ${countryDetail.Date} </small>
-              </div>
+              <p class="card-text">
+					<ul class="list-group">
+						  <li class="list-group-item">
+							Country Code: ${countryDetail.CountryCode}
+						  </li>
+						  <li class="list-group-item">
+							 <span class="badge bg-warning"> Confirmed :</span> <span class=" text-dark" > ${countryDetail.TotalConfirmed}</span>
+						  </li>
+						  <li class="list-group-item">
+							 <span class="badge bg-danger"> Deaths : </span> <span class=" text-dark">${countryDetail.TotalDeaths}</span>
+						  </li>
+						  <li class="list-group-item">
+							 <span class="badge bg-success"> Recovered : </span> <span class=" text-dark"> ${countryDetail.TotalRecovered} </span>
+						  </li>
+                    </ul>
+				</p>
+                  <div class="card-footer  text-center">
+					<small class="text-dark">Date :  ${countryDetail.Date} </small>
+				  </div>
             </div>
           </div>
-        </div>
-      </div>
+    </div>
 				`;
   });
 }
 getData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- ncovid2019Tracker-india/js/script.js
+++ ncovid2019Tracker-india/js/script.js
@@ -2,39 +2,37 @@ const API_URL = "https://api.covid19india.org/data.json";
 async function covidData() {   
    const response = await fetch(API_URL);
    const data = await response.json();
-
-const datajson = data.statewise[0];
-console.log(datajson)
+   const datajson = data.statewise[0];
     let countryDashboard = `
     <div class="pb-5 m-auto row row-cols-1 row-cols-md-4 g-4">
                     <div class="col">
-                      <div class="card border-3 border-secondary ">
+                      <div class="card cardborderone ">
                         <div class="text-center card-body">
-                          <h5 class="card-title">Confirmed </h5>
-                          <p class="card-text">${datajson.confirmed}</p>
+                          <h5 class="card-title text-uppercase">Confirmed </h5>
+                          <p class="card-text ">${datajson.confirmed}</p>
                         </div>
                       </div>
                     </div>
                     <div class="col">
-                      <div class="card border-3 border-warning">
+                      <div class="card cardbordertwo">
                         <div class="text-center card-body">
-                          <h5 class="card-title">Active </h5>
+                          <h5 class="card-title text-uppercase">Active </h5>
                           <p class="card-text">${datajson.active}</p>
                         </div>
                       </div>
                     </div>
                     <div class="col">
-                      <div class="card border-3 border-success">
+                      <div class="card cardborderthree">
                       <div class="text-center card-body">
-                          <h5 class="card-title">Recovered </h5>
+                          <h5 class="card-title text-uppercase">Recovered </h5>
                           <p class="card-text">${datajson.recovered}</p>
                         </div>
                       </div>
                     </div>
                     <div class="col">
-                      <div class="card border-3 border-danger">
+                      <div class="card cardborderfour">
                         <div class="text-center card-body">
-                          <h5 class="card-title">Deaths</h5>
+                          <h5 class="card-title text-uppercase">Deaths</h5>
                           <p class="card-text">${datajson.deaths}</p>
                         </div>
                       </div>
@@ -43,11 +41,121 @@ console.log(datajson)
                      <div class="pb-3 m-auto text-center card-footer bg-dark">
                       <small class="text-white ">Last updated : ${datajson.lastupdatedtime}</small>
                     </div>`;
-
 document.querySelector("#countryDashboard").innerHTML = countryDashboard;
 
 }
 covidData();
+
+
+const stateDashboard = document.querySelector("#stateDashboard");
+async function getData() {
+  stateDashboard.innerHTML = "";
+  const response = await fetch(`https://api.covid19india.org/data.json`);
+  const json = await response.json();
+  json.statewise.forEach((stateDetail) => {
+    stateDashboard.innerHTML += `
+    <div class="col-lg-4 col-md-3 g-3">
+      <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title text-center fw-bold">${stateDetail.state}</h5>
+              <p class="card-text">
+              <ul class="list-group">
+                      <li class="list-group-item">
+                        Country Code : ${stateDetail.statecode}
+                      </li>
+                       <li class="list-group-item">
+                        <span class="badge bg-secondary"> Active : </span> <span class=" text-dark"> ${stateDetail.active}</span>
+                      </li>
+                      <li class="list-group-item">
+                        <span class="badge bg-warning"> Confirmed : </span> <span class=" text-dark" > ${stateDetail.confirmed}</span>
+                      </li>
+                      <li class="list-group-item">
+                       <span class="badge bg-danger"> Deaths : </span> <span class="text-dark"> ${stateDetail.deaths}</span>
+                      </li>
+                      <li class="list-group-item">
+                        <span class="badge bg-success"> Recovered : </span> <span class=" text-dark"> ${stateDetail.recovered} </span>
+                      </li>
+                    </ul>
+					        </p>
+                  <div class="card-footer  text-center">
+                  <small class="text-dark fw-normal">Date :  ${stateDetail.lastupdatedtime} </small>
+              </div>
+            </div>
+          </div>
+    </div>
+
+				`;
+  });
+}
+getData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
